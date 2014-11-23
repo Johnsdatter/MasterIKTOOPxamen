e351180ceae81c639b0d5b2b0a63aa8c0c572c6b
Rune Holmdal <rholmdal@gmail.com>
2014-11-23 10:00:45 +0100

    Update by Rune 23.11

--- Laerer.java
+++ Laerer.java
@@ -1,31 +1,32 @@
-/* Dette er et objekt for hver enkelt tilgjengelige lærer, med navn, spesialkompetanse, oppgaver etc.
- * Brukes for å lagre tilgjengelige ressurser for bruk i fordelingsklassen.
- * Kodingen er utført av Jenny Emelia Nikolaisen
- * Dette er versjon nr. 1 (21.11.14)
+/* Dette er et objekt for hver enkelt tilgjengelige lÃ¦rer, med navn, spesialkompetanse, oppgaver etc.
+ * Brukes for Ã¥ lagre tilgjengelige ressurser for bruk i fordelingsklassen.
+ * Kodingen er utfoert av Jenny Emelia Nikolaisen
+ * Bearbeiding av Rune
+ * Dette er versjon nr. 2 (23.11.14)
 */
 
 public class Laerer
 {
-	private String laererNavn; //lærer som knyttes til kompetanse og oppgaver
-	private String spesialKompetanse; //fagene lærerne har kompetanse i
-	private String spesielleOppgaver; //eks kontaktlærer, faglærer, IKT-ansvarlig osv.
-	private int spesielleTimer; //som går til spesielle oppgaver
-	private int stillingsProsent; //hvor mange prosent stilling har læreren
+	private String laererNavn; //lÃ¦rer som knyttes til kompetanse og oppgaver
+	private String[] spesialKompetanse = new String[3]; //fagene lÃ¦rerne har kompetanse i, maks 3
+	private String[] spesielleOppgaver = new String[3]; //eks kontaktlÃ¦rer, faglÃ¦rer, IKT-ansvarlig osv., maks 3
+	private int spesielleTimer; //som gÃ¥r til spesielle oppgaver
+	private int stillingsProsent; //hvor mange prosent stilling har lÃ¦reren
 	private int tilgjengeligeTimer; //totalt tilgjengelige timer som kan fordeles
 
-	//konstruktør
+	//konstruktoer
 	public Laerer()
 	{
 		laererNavn = "";
-		spesialKompetanse = "";
-		spesielleOppgaver = "";
+		spesialKompetanse = {"", "", ""};
+		spesielleOppgaver = {"", "", ""};
 		spesielleTimer = 0;
 		stillingsProsent = 0;
 		tilgjengeligeTimer = 0;
 	}
 
-	//Konstruktør
-	public Laerer(String lN, String sK, String sO, int sT, int sP, int tT)
+	//KonstruktÃ¸r
+	public Laerer(String lN, String[] sK, String[] sO, int sT, int sP, int tT)
 	{
 		laererNavn = lN;
 		spesialKompetanse = sK;
@@ -35,34 +36,59 @@ public class Laerer
 		tilgjengeligeTimer = tT;
 	}
 
-	public String set(String )
+	//Gir mulighet for Ã¥ endre timetallet etterhvert som lÃ¦rer knyttes til oppgaver
+	//Sett fra hovedklassen er dette eneste variabel som krever set metode.
+	public String setTilgjengeligeTimer( int t )
 	{
-
+		tilgjengeligeTimer -= t;
 	}
 
-	public String getlaererNavn() // lager en get-metode som returnerer lærernavnet
+	public String getLaererNavn() // lager en get-metode som returnerer lÃ¦rernavnet
 	{
-			return ;
+		return laererNavn;
 	}
-	public String getspesialKompetanse() // lager en get-metode som returnerer
+	
+	public String[] getSpesialKompetanse() // lager en get-metode som returnerer spesialKompetanse som string array
 	{
-			return ;
+		return spesialKompetanse;
 	}
-	public String getspesielleOppgaver() // lager en get-metode som returnerer
+	
+	// lager en get-metode som returnerer verdien i posisjon gitt med int parameter indeks i string arrayet spesialKompetanse
+	public String getSpesialKompetanse(int indeks) 
 	{
-			return ;
+		if (indeks < 3)
+			return spesialKompetanse[indeks];
+		else
+			return "";
 	}
-	public String getspesielleTimer() // lager en get-metode som returnerer
+	
+	public String[] getSpesielleOppgaver() // lager en get-metode som returnerer
 	{
-			return ;
+		return spesielleOppgaver;
 	}
-	public String getstillingsProsent() // lager en get-metode som returnerer
+	
+	// lager en get-metode som returnerer verdien i posisjon gitt med int parameter indeks i string arrayet SpesielleOppgaver
+	public String getSpesielleOppgaver(int indeks)
 	{
-			return ;
+		if (indeks < 3)
+			return spesielleOppgaver[indeks];
+		else
+			return "";
 	}
-	public String gettilgjengeligeTimer() // lager en get-metode som returnerer
+	
+	public int getSpesielleTimer() // lager en get-metode som returnerer
 	{
-			return ;
+		return spesielleTimer;
+	}
+	
+	public int getStillingsProsent() // lager en get-metode som returnerer
+	{
+		return stillingsProsent;
+	}
+	
+	public int getTilgjengeligeTimer() // lager en get-metode som returnerer
+	{
+		return tilgjengeligeTimer;
 	}
 
-} // Slutt på klassen Laerer
+} // Slutt pÃ¥ klassen Laerer
