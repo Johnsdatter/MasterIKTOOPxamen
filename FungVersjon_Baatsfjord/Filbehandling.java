cec0a1b8fd69e0084eb54845852ef8fd514a0a53
Rune Holmdal <rholmdfal@gmail.com>
2014-11-24 23:43:25 +0100

    Gjeldende versjon av prosjekt fra 24.11.14?
    Sjekket og fungerer mot nye datafiler fra Båtsfjord :+1: NB! I Fag.java
    er metode public int tilgjengeligLaerer(Laerer[] laerer) kommentert ut
    da den gir kompileringsfeil. To variabelreferanser til Laerer krever
    omskriving, før den kan kommenteres inn igjen. Ellers skulle det være
    klart for å teste ut fordelings-algoritmen.

---
+++ FungVersjon_Baatsfjord/Filbehandling.java
@@ -0,0 +1,197 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+public class Filbehandling
+{
+	// Finner antall linjer i filen.
+	public int antLinjer(String filNavn)
+	{
+		BufferedReader br = null;
+		String lestData = "";
+		int antLinjer = 0;
+		try
+		{
+			br = new BufferedReader( new FileReader(filNavn) );
+			while( ( lestData = br.readLine() ) != null )
+			{
+				antLinjer++;
+			}
+		}
+		catch (IOException e)
+		{
+			JOptionPane.showMessageDialog(null, "",
+				e.toString(), JOptionPane.PLAIN_MESSAGE );
+		}
+		finally
+		{
+			if ( br != null )
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					JOptionPane.showMessageDialog(null, "",
+						e.toString(), JOptionPane.PLAIN_MESSAGE );
+				}
+			}
+			return antLinjer;
+		}
+	}
+
+	// Fyller et array objekt av klassen Fag med innhold fra fil
+	public Fag[] lesFagRessurs(String filNavn, Fag[] f)
+	{
+		String lestData = "";
+		BufferedReader br = null;
+		int i = 0;
+		try
+		{
+			br = new BufferedReader( new FileReader(filNavn) );
+			while( ( lestData = br.readLine() ) != null )
+			{
+				f[i] = new Fag( lestData, 0 );
+				i++;
+			}
+		}
+		catch (IOException e)
+		{
+			JOptionPane.showMessageDialog(null, "",
+				e.toString(), JOptionPane.PLAIN_MESSAGE );
+		}
+		finally
+		{
+			if ( br != null )
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					JOptionPane.showMessageDialog(null, "",
+						e.toString(), JOptionPane.PLAIN_MESSAGE );
+				}
+			}
+			return f;
+		}
+	}
+
+	// Fyller et array objekt av klassen Aarstreinn med innhold fra fil
+	public Aarstrinn[] lesAarstrinnRessurs(String filNavn, Aarstrinn[] trinn)
+	{
+		String lestData = "";
+		BufferedReader br = null;
+		int i = 0;
+		try
+		{
+			br = new BufferedReader( new FileReader(filNavn) );
+			while( ( lestData = br.readLine() ) != null )
+			{
+				String[] data = lestData.split(";");
+				String[] f = new String[data.length/2];
+				int[] t = new int [data.length/2];
+				int y = 0;
+				for ( int x = 0; x < data.length; x+=2 )
+				{
+					f[y] = data[x];
+					t[y] = Integer.parseInt( data[x+1] );
+					y++;
+				}
+				trinn[i] = new Aarstrinn( f, t );
+				i++;
+			}
+
+		}
+		catch (IOException e)
+		{
+			JOptionPane.showMessageDialog(null, "",
+				e.toString(), JOptionPane.PLAIN_MESSAGE );
+		}
+		finally
+		{
+			if ( br != null )
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					JOptionPane.showMessageDialog(null, "",
+						e.toString(), JOptionPane.PLAIN_MESSAGE );
+				}
+			}
+			return trinn;
+		}
+	}
+
+	// Fyller et array objekt av klassen Laerer med innhold fra fil
+	public Laerer[] lesLaererRessurs(String filNavn, Laerer[] laererRessurs)
+	{
+		String lestData = "";
+		String navn = "";
+		String[] kompetanse = new String[3];
+		String[] oppgaver = new String[3];
+		int spesielleTimer;
+		int stillingsProsent;
+		int tilgjengeligeTimer;
+		BufferedReader br = null;
+		int i = 0;
+
+		String s = "filinnhold ikke lest";
+		try
+		{
+			br = new BufferedReader( new FileReader(filNavn) );
+			while( ( lestData = br.readLine() ) != null )
+			{
+				String[] data = lestData.split(";");
+				navn = data[0];
+				kompetanse[0] = data[1];
+				kompetanse[1] = data[2];
+				kompetanse[2] = data[3];
+				oppgaver[0] = data[4];
+				oppgaver[1] = data[5];
+				oppgaver[2] = data[6];
+				spesielleTimer = Integer.parseInt( data[7] );
+				stillingsProsent = Integer.parseInt( data[8] );
+				tilgjengeligeTimer = Integer.parseInt( data[9] );
+				laererRessurs[i] = new Laerer( navn, kompetanse, oppgaver, spesielleTimer, stillingsProsent, tilgjengeligeTimer );
+				i++;
+			}
+		}
+		catch (IOException e)
+		{
+			JOptionPane.showMessageDialog(null, "",
+				e.toString(), JOptionPane.PLAIN_MESSAGE );
+		}
+		finally
+		{
+			if ( br != null )
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					JOptionPane.showMessageDialog(null, "",
+						e.toString(), JOptionPane.PLAIN_MESSAGE );
+				}
+			}
+			return laererRessurs;
+		}
+
+	}
+
+	// Lokal metode for å sende ut debug informasjon
+	private void debugMelding( String m )
+	{
+		JOptionPane.showMessageDialog(null, m, "Fra filbehandling", JOptionPane.PLAIN_MESSAGE );
+	}
+
+}
---
+++ FungVersjon_Baatsfjord/Ressursfordeling.java
@@ -0,0 +1,101 @@
+/***** Oppgave Ressursfordeling ********
+* Hovedklasse
+* Programmet skal ta i bruk to datasett,
+* ett med skolens behov for timefordeling pr. fag pr. årstrinn og
+* ett med tilgjengelige ressurser
+* (dvs. tilgjengelige lærere med ulik arbeidstid og kompetanse).
+* Ressursene lagres i klassene Lærer, Årstrinn og Fag, for å bli hentet inn
+* i klassen Fordeling som leser inn skolens behov og fordeler ressursene i
+* henhold til disse. Resultatet av fordelinga skal så lagres og visualiseres
+* i klassen Fordeling.
+
+< Opprette array av klassene årstrinn-, fag - og lærer  >
+< Lese fra fil for å definere array størrelser og hente data til hvert objekts datafelter.
+	Mulig utvidelse: Lage bruker-input >
+
+< Fordele timer og ressurser. Gjøres i/av klassen Fordeling >
+	< Opprette Fordelings-objekt og utføre nødvendige kall på dets metoder >
+
+< Gjøre kall på Fordelings-objektets metoder for å skrive oversikter til fil/dialogboks >
+
+*/
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+public class Ressursfordeling
+{
+	public static void main( String[] args )
+	{
+		int maksoppfoeringer = 25; //Samme som i klassen Aarstrinn
+		String aarstrinnFilen = "aarstrinnRessurs.csv";
+		String laererFilen = "lærerRessurs.csv";
+		String fagFilen = "fagRessurs.csv";
+
+		// For å få tilgang til metodene i klassen Filbehandling
+		Filbehandling fil = new Filbehandling();
+
+		//**** Leser filene for å dimmensjonere arrayene av ressursklassene.
+		int antAarstrinn = fil.antLinjer( aarstrinnFilen );
+		int antFag = fil.antLinjer( fagFilen );
+		int antLaerer = fil.antLinjer( laererFilen );
+
+		//lager array-objekter av ressursklassene
+		Aarstrinn[] trinnRessurs = new Aarstrinn[ antAarstrinn ];
+		Fag[] fagRessurs = new Fag[ antFag ];
+		Laerer[] laererRessurs = new Laerer[ antLaerer ];
+
+		//**** Fyller objektene med informasjonen i datafilene
+		trinnRessurs = fil.lesAarstrinnRessurs( aarstrinnFilen, trinnRessurs );
+		fagRessurs = fil.lesFagRessurs( fagFilen, fagRessurs );
+		laererRessurs = fil.lesLaererRessurs( laererFilen, laererRessurs );
+
+
+		// ****** Test løkke for å lese ut trinnRessurs registrereinger
+		for( int x = 0; x < antAarstrinn; x++)
+		{
+			String s = "";
+			for ( int y = 0; y < 11; y++ )
+			{
+					s += trinnRessurs[x].getFag(y) + ", " + trinnRessurs[x].getTimer(y) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, s, "Årstrinn", JOptionPane.PLAIN_MESSAGE );
+		}
+
+/*********************************************************************************************
+*************                  TESTER INNHOLD I OBJEKTENE                      ***************
+*************        trinnRessurs, fagRessurs og laererRessurs                 ***************
+**********************************************************************************************/
+		// ****** Test løkke for å lese ut fagRessurs registrereinger
+		String s = "";
+		for( int x = 0; x < antFag; x++)
+		{
+				s += fagRessurs[x].getfagNavn() + "\n";
+		}
+		JOptionPane.showMessageDialog(null, s, "Fag", JOptionPane.PLAIN_MESSAGE );
+
+		// ****** Test løkke for å lese ut laererRessurs registrereinger
+		s = "";
+		for( int x = 0; x < antLaerer; x++) //
+		{
+			s = laererRessurs[x].getLaererNavn() + "\n" +
+				laererRessurs[x].getSpesialKompetanse(0) + "\n" +
+				laererRessurs[x].getSpesialKompetanse(1) + "\n" +
+				laererRessurs[x].getSpesialKompetanse(2) + "\n" +
+				laererRessurs[x].getSpesielleOppgaver(0) + "\n" +
+				laererRessurs[x].getSpesielleOppgaver(1) + "\n" +
+				laererRessurs[x].getSpesielleOppgaver(2) + "\n" +
+				laererRessurs[x].getSpesielleTimer() + "\n" +
+				laererRessurs[x].getStillingsProsent() + "\n" +
+				laererRessurs[x].getTilgjengeligeTimer();
+			JOptionPane.showMessageDialog(null, s, "Lærer", JOptionPane.PLAIN_MESSAGE );
+		}
+		laererRessurs[2].setTilgjengeligeTimer( 3 );
+		s = "" + laererRessurs[2].getTilgjengeligeTimer();
+		JOptionPane.showMessageDialog(null, "Endring av timetall:\n Opprinnelige timetall er 21 og reduseres med 3:\n Gjeldende timetall er: " + s, "Lærer", JOptionPane.PLAIN_MESSAGE );
+	}
+//********************************** TEST SLUTT ***********************************************
+
+}
