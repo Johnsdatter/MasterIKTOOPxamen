18b41919ebfa127bf3708b9b684a235f9069d14b
Emelia80 <jenny.emelia.nikolaisen@hotmail.com>
2014-11-25 20:12:09 +0100

    25.11.14 by Jenny

--- FungVersjon_Baatsfjord/Fag.java
+++ FungVersjon_Baatsfjord/Fag.java
@@ -1,6 +1,6 @@
 /* Denne klassen samler tilgjengelige ressurser per faggruppe, uavhengig av trinnfordeling. Klassen brukes i fordelinga.
 * Kodingen er utført av Jenny Emelia Nikolaisen
-* Dette er versjon nr. 5 (17.11.14)
+* Dette er versjon nr. 6 (25.11.14)
 */
 
 public class Fag
@@ -35,24 +35,31 @@ public class Fag
 	{
 		return fagNavn;
 	}
-	/* må kommentere ut denne så lenge siden objektet laerer ikke er opprettet ennå
+	/* må kommentere ut denne så lenge siden objektet laerer ikke er opprettet ennå */
+
 	public String getLaerere() // lager en get-metode som returnerer lærere
 	{
 		return laerere;
 	}
-	*/
+
+	public int getlaererIndeks() // lager en get-metode som returnerer lærerindeksen
+	{
+		return laererIndeks;
+	}
+
 
 /* Måte kommentere den ut da den gir kompileringsfeil på laerer[i].fordypning som ikke eksisterer.
    Antar at den refererer til arrayet spesialKompetanse[n] i Laerer
-   Dermed må den omskrives litt
-		public int tilgjengeligLaerer(Laerer[] laerer) // Returnerer indeks til mest tilgjengelige lærer. Public fordi Andreas skal ha tilgang.
+   Dermed må den omskrives litt*/
+
+		public void tilgjengeligLaerer() // Returnerer indeks til mest tilgjengelige lærer. Public fordi Andreas skal ha tilgang.
 		{
 			int laererIndeks = 0;
 			int flestTimer = 0;
 
 			for (int i = 0 ; i < laerer.length ; i++) // Går igjennom alle lærerne
 			{
-				if (laerer[i].fordypning == fagNavn) // muligens endres, se Trudes koding for eksakte navn
+				if (laerer[i].spesialKompetanse == fagNavn) // muligens endres, se Trudes koding for eksakte navn
 				{
 
 					if (laerer[i].timer > flestTimer)
@@ -62,9 +69,12 @@ public class Fag
 					}
 				}
 			}
-			return laererIndeks;
 
-	}	// Slutt på tilgjengeligLaerer
-*/
+		}	// Slutt på tilgjengeligLaerer
+
+	public int gettilgjengeligLaerer () // lager en get-metode som returnerer tilgjengelige lærere
+	{
+		return tilgjengeligLaerer;
+	}
 
 } // Slutt på klassen Fag
